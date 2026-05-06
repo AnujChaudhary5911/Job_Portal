36f54b0da15f429608458fd89150d0bde6d96409
Anuj <your-github-anujchh287@gmail.com>
2026-05-06 21:02:29 +0530

    same issue

--- backend/config/email.js
+++ backend/config/email.js
@@ -2,7 +2,9 @@ import nodemailer, { createTransport } from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
  export const Transport=createTransport({
-    service:'gmail',
+    host: 'smtp.gmail.com',  // Service ki jagah direct host ka naam diya
+    port: 465,               // Secure port
+    secure: true,
     auth:{
         user:process.env.EMAIL_USER,
         pass:process.env.EMAIL_PASS
